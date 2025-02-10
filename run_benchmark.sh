55ce515d40b77772256df8b49200cc246defaac0
Alejorotp <98970394+Alejorotp@users.noreply.github.com>
2025-02-10 02:51:30 -0500

    Final Commit

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Verifica si la carpeta ya existe para evitar descargas innecesarias
 rm -rf solutions
 if [ ! -d "solutions" ]; then
     echo "Descargando repositorio con soluciones..."
@@ -10,22 +9,18 @@ else
     echo "Repositorio ya descargado. Usando soluciones existentes..."
 fi
 
-# Reiniciar archivo de resultados
 echo "" > results.txt
 
 echo "Ejecutando soluciones en distintos lenguajes..."
 for lang in solutions/*; do
     if [ -d "$lang" ]; then
-        lang_name=$(basename "$lang")  # Extrae solo el nombre del lenguaje
+        lang_name=$(basename "$lang")
         echo "Ejecutando $lang_name..."
 
-        # Construir la imagen
         docker build -t "${lang_name}_benchmark" "$lang"
 
-        # Ejecutar el contenedor y guardar el tiempo
         TIME_OUTPUT=$(docker run --rm "${lang_name}_benchmark")
 
-        # Guardar el resultado en el archivo
         echo "$lang_name: $TIME_OUTPUT" >> results.txt
     fi
 done
